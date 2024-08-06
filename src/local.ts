22d104d17c93d12214e05f881bab6f581e81d3f4
Binh Bui <bvbinh@sk-global.biz>
2024-08-06 16:59:21 +0700

    chore: Update local.ts to save muniMap to file

--- src/local.ts
+++ src/local.ts
@@ -1,5 +1,9 @@
 import { getMuniMap } from './muni';
-import { latLonToAddressInfo, searchAddress, latLonToAddress } from './index';
+import {
+  latLonToAddressInfo,
+  searchAddress,
+  latLonToAddress,
+} from './index';
 
 import * as fs from 'fs';
 
@@ -8,16 +12,16 @@ const run = async () => {
   // save to file
   fs.writeFileSync('data/muni.json', JSON.stringify(muniMap, null, 2));
 
-  const latlon = [37.304482,137.148414];
+  const latlon = [35.6895, 139.6917];
   const [lat, lon] = latlon;
 
   const result = await latLonToAddress(lat, lon);
 
   console.log(result);
 
-  // const q = '北海道';
-  // const searchResults = await searchAddress(q);
-  // console.log(searchResults);
+  const q = '北海道';
+  const searchResults = await searchAddress(q);
+  console.log(searchResults);
 };
 
 run();
